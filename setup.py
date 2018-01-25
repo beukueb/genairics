b195595de37e2bf208b3715c180e87f5294c33f3
Christophe Van Neste <christophe.vanneste@ugent.be>
2018-01-25 08:28:13 +0100

    version

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 package = "genairics"
-version = 0.1.25
+version = "0.1.25"
 
 setup(name = package,
-      version = 0.1.25
+      version = version
       description="GENeric AIRtight omICS pipelines",
       url='https://github.com/beukueb/genairics',
       author = 'Christophe Van Neste',
