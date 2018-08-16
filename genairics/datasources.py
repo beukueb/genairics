e74e8d2afd1197cdc5cc26458506b2755dd0a5ea
Christophe Van Neste <christophe.vanneste@kaust.edu.sa>
2018-08-16 11:33:54 +0300

    data source real path

--- genairics/datasources.py
+++ genairics/datasources.py
@@ -47,6 +47,7 @@ Providers: `file`, `basespace`
         # Data source cases:
         if provider == 'file':
             # In case the data is a local file path, the directory is linked to the datadir
+            location = os.path.realpath(location)
             os.symlink(location,self.output().path)
         elif provider == 'basespace':
             bssource = BaseSpaceSource(
